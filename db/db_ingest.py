32b66db0c4e8cdd117bc65a363db9d436835581f
Kleber Castro <klebervic90@gmail.com>
2025-09-29 01:56:38 -0300

    Responde partes 1 (Criar Banco de Dados), 2 (Estrutura de Arquivos) e 3 (Script de Ingestão) da etapa 3

---
+++ db/db_ingest.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+import os
+
+path_csv = 'data/processed/filmes_limpos.csv'
+path_db = 'db/filmes.db'
+
+os.makedirs('db', exist_ok=True)
+
+engine = create_engine(f'sqlite:///{path_db}') #gerenciador de conexão com o banco de dados (objeto que sabe como se conectar ao banco de dados).
+
+df = pd.read_csv(path_csv)
+
+df.to_sql('filmes', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as conn:
+    result = conn.execute(text("PRAGMA table_info(filmes)"))
+
+print("Esquema da tabela 'filmes':")
+for row in result:
+    print(f"Nome: {row[1]}, Tipo: {row[2]}")

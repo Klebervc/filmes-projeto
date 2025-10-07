94ff8c815921ff49742ee73420328bd39e4b331b
Kleber Castro <klebervic90@gmail.com>
2025-10-07 16:05:01 -0300

    Faz error handling do arquivo db_ingest.py

--- db/db_ingest.py
+++ db/db_ingest.py
@@ -4,20 +4,46 @@ import os
 
 def processar_filmes(path_csv='data/processed/filmes_limpos.csv', path_db='db/filmes.db'):
     
-    os.makedirs(os.path.dirname(path_db), exist_ok=True)
+    try:
+        os.makedirs(os.path.dirname(path_db), exist_ok=True)    
+    except Exception as dir_error:
+        print(f'Erro na criação do diretório da base de dados: {dir_error}')
+        return
     
-    engine = create_engine('sqlite:///{path_db}')
+    try:    
+        engine = create_engine(f'sqlite:///{path_db}')
+    except Exception as engine_error:
+        print(f'Erro na criação da engine: {engine_error}')
+        return
     
-    df = pd.read_csv(path_csv)
+    try:
+        df = pd.read_csv(path_csv)
+    except FileNotFoundError:
+        print(f'O arquivo CSV não existe: {path_csv}')
+        return
+    except pd.errors.ParserError as read_error:
+        print(f'Erro ao ler o arquivo CSV: {read_error}')
+        return
+    except Exception as variant_error:
+        print(f'Erro ao carregar CSV: {variant_error}')
+        return
     
-    df.to_sql('filmes', con=engine, if_exists='replace', index=False)
+    try:
+        df.to_sql('filmes', con=engine, if_exists='replace', index=False)
+    except Exception as salvar_tabela:
+        print(f'Erro ao salvar tabela: {salvar_tabela}')
+        return
     
-    with engine.connect() as cnn:
-        result = cnn.execute(text("PRAGMA table_info(filmes)"))
+    try:
+        with engine.connect() as cnn:
+            result = cnn.execute(text("PRAGMA table_info(filmes)"))
     
-    print("Informações da tabela 'filmes':")
-    for row in result:
-        print(f"Nome: {row[1]}, Tipo: {row[2]}")
+        print("Informações da tabela 'filmes':")
+        for row in result:
+            print(f"Nome: {row[1]}, Tipo: {row[2]}")
+    except Exception as consulta_SQL:
+        print(f'Erro ao realizar consulta SQL: {consulta_SQL}')
+        return
     
     return engine
 
